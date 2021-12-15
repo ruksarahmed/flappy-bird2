543d3400e9f05c6855601987ec5f5c66e0185cf9
Ruksar Ahmed <ruksar.org@gmail.com>
2021-12-15 23:54:34 +0530

    First Commit

---
+++ lib/coverscreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyCoverScreen extends StatelessWidget {
+  const MyCoverScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/homePage.dart
@@ -0,0 +1,233 @@
+import 'dart:async';
+import 'package:flappybird2/bird.dart';
+import 'package:flutter/material.dart';
+
+import 'barrier.dart';
+import 'coverscreen.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // bird variables
+  static double birdY = 0;
+  double initialPos = birdY;
+  double height = 0;
+  double time = 0;
+  double gravity = -4.9; //How strong the gravity is
+  double velocity = 3.5; //How strong the jump is
+  double birdWidth = 0.1; // out of 2, 2 being the entire width of the screen
+  double birdHeight = 0.1; // out of 2, 2 being the entire height of the screen
+
+  // game settings
+  bool gameHasStarted = false;
+
+  // barrier variables
+  static List<double> barrierX = [2, 2 + 1.5];
+  static double barrierWidth = 0.5; // out of two
+  List<List<double>> barrierHeight = [
+    // out of 2, where 2 is the entire height of the screen
+    // [topHeight, bottomHeight]
+    [0.6, 0.4],
+    [0.4, 0.6],
+  ];
+
+  void startGame() {
+    gameHasStarted = true;
+    Timer.periodic(Duration(milliseconds: 100), (timer) {
+      // a real physical jump is the same as an upside down parabola
+      // so this is a simple quadratic equation
+      height = gravity * time * time + velocity * time;
+
+      setState(() {
+        birdY = initialPos - height;
+      });
+
+      // check if bird is dead
+      if (birdIsDead()) {
+        timer.cancel();
+        _showDialog();
+      }
+
+      // keep the map moving (move barriers)
+      moveMap();
+
+      // keep the time going
+      time += 0.1;
+    });
+  }
+
+  void moveMap() {
+    for (int i = 0; i < barrierX.length; i++) {
+      // keep barriers moving
+      setState(() {
+        barrierX[i] -= 0.005;
+      });
+
+      // if barrier exits the left part of the screen, keep it looping
+      if (barrierX[i] < -1.5) {
+        barrierX[i] += 3;
+      }
+    }
+  }
+
+  void resetGame() {
+    Navigator.pop(context);
+    setState(() {
+      birdY = 0;
+      gameHasStarted = false;
+      time = 0;
+      initialPos = birdY;
+    });
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.brown,
+          title: Center(
+            child: Text(
+              'G A M E   O V E R',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          actions: <Widget>[
+            GestureDetector(
+              onTap: resetGame,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: Container(
+                  padding: EdgeInsets.all(7),
+                  color: Colors.white,
+                  child: Text(
+                    'PLAY AGAIN',
+                    style: TextStyle(
+                      color: Colors.brown,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void jump() {
+    setState(() {
+      time = 0;
+      initialPos = birdY;
+    });
+  }
+
+  bool birdIsDead() {
+    // check if bird is hitting the top or the bottom of the screen
+    if (birdY < -1 || birdY > 1) {
+      return true;
+    }
+
+    // hits barriers
+    // check if bird is within x coordinates and y coordinates of barriers
+    for (int i = 0; i < barrierX.length; i++) {
+      if (barrierX[i] <= barrierWidth &&
+          barrierX[i] + barrierWidth >= -birdWidth &&
+          (birdY <= -1 + barrierHeight[i][0] ||
+              birdY + birdHeight >= 1 - barrierHeight[i][1])) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: gameHasStarted ? jump : startGame,
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.blue,
+                child: Center(
+                  child: Stack(
+                    children: [
+                      MyBird(
+                        birdY: birdY,
+                        birdWidth: birdWidth,
+                        birdHeight: birdHeight,
+                      ),
+
+                      // tap to play
+                      // MyCoverScreen(
+                      //   gameHasStarted: gameHasStarted,
+                      // ),
+
+                      // top barrier 0
+                      MyBarrier(
+                        barrierX: barrierX[0],
+                        barrierWidth: barrierWidth,
+                        barrierHeight: barrierHeight[0][0],
+                        isThisBottomBarrier: false,
+                      ),
+
+                      // bottom barrier 0
+                      MyBarrier(
+                        barrierX: barrierX[0],
+                        barrierWidth: barrierWidth,
+                        barrierHeight: barrierHeight[0][1],
+                        isThisBottomBarrier: true,
+                      ),
+
+                      // top barrier 1
+                      MyBarrier(
+                        barrierX: barrierX[1],
+                        barrierWidth: barrierWidth,
+                        barrierHeight: barrierHeight[1][0],
+                        isThisBottomBarrier: false,
+                      ),
+
+                      // bottom barrier 1
+                      MyBarrier(
+                        barrierX: barrierX[1],
+                        barrierWidth: barrierWidth,
+                        barrierHeight: barrierHeight[1][1],
+                        isThisBottomBarrier: true,
+                      ),
+
+                      Container(
+                        alignment: Alignment(0, -0.5),
+                        child: Text(
+                          gameHasStarted ? ' ' : 'T A P  T O  P L A Y',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(color: Colors.brown),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
